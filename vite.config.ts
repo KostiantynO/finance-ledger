91897d6f48c1acd98cb41ae7d03380fe5aea711d
Kostiantyn Ochenash <Ochenashk@gmail.com>
2023-01-28 23:16:06 +0200

    removes unused var

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
-import eslint from '@rollup/plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
